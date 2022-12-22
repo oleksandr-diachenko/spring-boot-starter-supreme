06088dd018a2cb206c435b5062434494bf78f75a
Oleksandr Diachenko <mail.positiv@gmail.com>
2022-12-22 18:56:14 +0200

    add junit test for AspectLoggerLookup

---
+++ src/test/java/com/epam/methodlog/aspect/lookup/AspectLoggerLookupTest.java
@@ -0,0 +1,37 @@
+package com.epam.methodlog.aspect.lookup;
+
+import com.epam.methodlog.tags.Junit;
+import org.aspectj.lang.JoinPoint;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@Junit
+@ExtendWith(MockitoExtension.class)
+class AspectLoggerLookupTest {
+
+    @InjectMocks
+    private AspectLoggerLookup aspectLoggerLookup;
+
+    @Mock
+    private JoinPoint joinPoint;
+
+    @Test
+    void shouldReturnLogger() {
+        when(joinPoint.getTarget()).thenReturn(new Target());
+
+        Logger logger = aspectLoggerLookup.lookup(joinPoint);
+
+        Logger expected = LoggerFactory.getLogger(Target.class);
+        assertThat(logger).isSameAs(expected);
+    }
+
+    private static class Target {}
+}
