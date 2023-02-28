191d96bda5d83ac03655d482488ecfd41fe6a200
Oleksandr Diachenko <mail.positiv@gmail.com>
2023-02-28 15:51:32 +0200

    add template validator to context

--- src/main/java/com/github/oleksandrdiachenko/supreme/MethodLogAutoConfig.java
+++ src/main/java/com/github/oleksandrdiachenko/supreme/MethodLogAutoConfig.java
@@ -1,11 +1,16 @@
 package com.github.oleksandrdiachenko.supreme;
 
+import com.github.oleksandrdiachenko.supreme.internal.bean.AnnotationMessageValidatorBPP;
 import com.github.oleksandrdiachenko.supreme.property.SupremeProperty;
 import com.github.oleksandrdiachenko.supreme.utils.formatter.MapWithoutBracketsStringFormatter;
 import com.github.oleksandrdiachenko.supreme.utils.formatter.StringFormatter;
+import com.github.oleksandrdiachenko.supreme.validator.Validator;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.*;
 
+import java.lang.reflect.Method;
+import java.util.List;
 import java.util.Map;
 
 @Configuration
@@ -18,6 +23,11 @@ public class MethodLogAutoConfig {
     @Bean
     public StringFormatter<Map<Integer, Object>> mapStringFormatter() {
         return new MapWithoutBracketsStringFormatter<>();
+    }
+
+    @Bean
+    public BeanPostProcessor annotationMessageValidatorBPP(List<Validator<Method>> validators) {
+        return new AnnotationMessageValidatorBPP(validators);
     }
 
 }
